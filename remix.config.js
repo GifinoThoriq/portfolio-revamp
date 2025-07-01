4afff6b5cd2f07fafa596608f6c9ab97930cbe43
Gifino Thoriq <gifinothoriq@Gifinos-MacBook-Air.local>
2025-07-01 13:59:18 +0700

    chore: build configuration

--- remix.config.js
+++ remix.config.js
@@ -1,7 +1,6 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
     serverBuildTarget: "vercel",
-    server: "./server-vercel.js", // you'll create this file next
     ignoredRouteFiles: ["**/.*"], // default
     // other configs if any...
   };
